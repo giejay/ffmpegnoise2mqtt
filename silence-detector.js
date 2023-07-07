bd926e4a74d9121e8353cbd0f6dbccd0e8ab1554
gj <gj@haos-helpers.haos-helpers.local>
2023-07-07 08:27:30 -0500

    add timeout with retry

--- silence-detector.js
+++ silence-detector.js
@@ -8,49 +8,56 @@ let lastSent = 0;
 
 getMeanVolume(STREAM_URL, function callback(meanVolume){
   console.log('volume', meanVolume)
-  if(Math.abs(lastSent - meanVolume) > 5){
-    console.log('Sending volume to MQTT');
-    if(client.connected){
-      client.publish('audio-detector/' + process.env.NAME, '' + meanVolume);
-      console.log('Published message');
-    } else {
-      throw Error('Client disconnected!');
+  if(meanVolume){
+    if(Math.abs(lastSent - meanVolume) > 5){
+      console.log('Sending volume to MQTT');
+      if(client.connected){
+        client.publish('audio-detector/' + process.env.NAME, '' + meanVolume);
+        console.log('Published message');
+      } else {
+        throw Error('Client disconnected!');
+      }
+      lastSent = meanVolume;
     }
-    lastSent = meanVolume;
   }
   getMeanVolume(STREAM_URL, callback);
 });
 
 function getMeanVolume(streamUrl, callback){
   console.log('Getting volume!');
- new Ffmpeg({ source: streamUrl })
-  .withAudioFilter('volumedetect')
-  .addOption('-f', 'null')
-  .addOption('-t', '3') // duration
-  .noVideo()
-  .on('start', function(ffmpegCommand){
-    // console.log('Output the ffmpeg command', ffmpegCommand);
-  })
-  .on('end', function(stdout, stderr){
-   
-   // find the mean_volume in the output
-    let meanVolumeRegex = stderr.match(/mean_volume:\s(-?\d*(\.\d+)?)/);
+  try {
+    new Ffmpeg({ source: streamUrl, timeout: 30 })
+      .withAudioFilter('volumedetect')
+      .addOption('-f', 'null')
+      .addOption('-t', '5') // duration
+      .noVideo()
+      .on('start', function(ffmpegCommand){
+        // console.log('Output the ffmpeg command', ffmpegCommand);
+      })
+      .on('end', function(stdout, stderr){
+      
+      // find the mean_volume in the output
+        let meanVolumeRegex = stderr.match(/mean_volume:\s(-?\d*(\.\d+)?)/);
 
-    console.log(stderr, meanVolumeRegex);
-   
-    // return the mean volume
-    if(meanVolumeRegex){
-      let meanVolume = parseFloat(meanVolumeRegex[1]);
-      return callback(meanVolume);
-    }
-   
-    // if the stream is not available
-    if(stderr.match(/Server returned 404 Not Found/)){
-      return callback(false);
-    }
+        console.log(stderr, meanVolumeRegex);
+      
+        // return the mean volume
+        if(meanVolumeRegex){
+          let meanVolume = parseFloat(meanVolumeRegex[1]);
+          return callback(meanVolume);
+        }
+      
+        // if the stream is not available
+        if(stderr.match(/Server returned 404 Not Found/)){
+          return callback(false);
+        }
 
-    console.log('regex not matched');
-    return callback(-100)
- })
- .saveToFile('/dev/null');
+        console.log('regex not matched');
+        return callback(false);
+    })
+    .saveToFile('/dev/null');
+  } catch(e){
+      console.error('Could not fetch volume, retrying', e);
+      callback(false)
+  }
 }
